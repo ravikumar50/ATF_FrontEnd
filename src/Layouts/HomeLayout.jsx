f0dba941aed3cbc22bfab1b627720ce6c186acb6
Kumar <ravi.fu.kumar@accenture.com>
2025-06-16 16:10:59 +0530

    Added some text in home page

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -90,7 +90,7 @@ function HomeLayout({children}){
                         
                         <li className="absolute bottom-4 w-[90%]">
                             <div className="w-full flex items-center justify-center">
-                            <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-full text-center" onClick={() => navigate("/")}>
+                            <button className="btn btn-secondary px-4 py-1 font-semibold rounded-md w-full text-center" onClick={() => navigate("/logout")}>
                                 Logout 
                             </button>
                             </div>
