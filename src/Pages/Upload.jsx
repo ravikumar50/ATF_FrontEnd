1d1c7fe8254fd9cff31167b704f9d11d4559fa14
Kumar <ravi.fu.kumar@accenture.com>
2025-06-16 23:26:52 +0530

    Change the visibility of the shaddow

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -55,7 +55,7 @@ function Upload() {
         <>
         <HomeLayout>
             <div className="flex items-center justify-center h-[90vh]">
-                <div className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]">
+                <div className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_20px_rgba(0,0,0,0.5)]">
                     <h1 className="text-center text-2xl font-bold ">Upload File</h1>
 
                     <div className="flex flex-col gap-1">
