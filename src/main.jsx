00edb51eb292d62322f948f1ba872faff6626775
Kumar <ravi.fu.kumar@accenture.com>
2025-06-19 23:14:27 +0530

    Added PersistGate to hold the count of overall passed, failed and skipped, so that on closing the app, we do not loose the data

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './Redux/store.js'
-import { Toaster } from 'react-hot-toast'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './Redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'react-hot-toast';
 
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
@@ -14,7 +15,6 @@ const msalConfig = {
     clientId: "3bd9f365-c333-4590-9b07-f6cd62fa936b",
     authority: "https://login.microsoftonline.com/c6326554-6b7c-4df2-85c1-d8f8c3824de9",
     redirectUri: "https://demonewlook-drftaegrg5gyeyff.canadacentral-01.azurewebsites.net"
-    
   }
 };
 
@@ -23,11 +23,12 @@ const msalInstance = new PublicClientApplication(msalConfig);
 createRoot(document.getElementById('root')).render(
   <MsalProvider instance={msalInstance}>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <Toaster></Toaster>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+          <Toaster />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </MsalProvider>
-  
-)
+);
