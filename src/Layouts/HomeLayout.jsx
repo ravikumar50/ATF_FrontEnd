c6d4c337ef00a0902c84f40bb122a460a05c3003
Kumar <ravi.fu.kumar@accenture.com>
2025-07-03 13:27:33 +0530

    Changed some part in home page

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -9,29 +9,23 @@ function HomeLayout({ children }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = async (e) => {
-    e.preventDefault();
-    const res = await dispatch(logoutFromAccount());
-    if (res?.payload?.success) {
-      navigate("/");
-    }
-  };
+  
 
   return (
-    <div className="h-[100vh] flex flex-col text-white">
+    <div className="h-[90vh] flex flex-col text-white">
       {/* Navbar */}
-      <nav className="bg-[#0b1120] shadow-lg">
-        <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+      <nav className="bg-[#0b1120] w-full">
+        <div className="w-full px-10 py-3 flex justify-between items-center">
           <Link to="/" className="text-2xl font-bold text-white">
             ATF Portal
           </Link>
 
-          <div className="space-x-6 font-bold text-sm text-white">
+          <div className="flex items-center justify-center gap-6 font-bold text-md text-white">
             <Link to="/" className="hover:text-[#00BBFF]">Home</Link>
             <Link to="/allProjects" className="hover:text-[#00BBFF]">View Projects</Link>
             {isAuthenticated && (
               <button
-                onClick={handleLogout}
+                onClick={()=>{navigate('/logout')}}
                 className="bg-yellow-500 text-black px-3 py-1 rounded hover:bg-yellow-600 transition"
               >
                 Logout
